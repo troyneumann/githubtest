876eb77677c40ee50624cc482dc9ebcc703d57fd
Troy Neuman <neumann.troy@mayo.edu>
2018-02-16 12:28:12 -0600

    Initial Commit

---
+++ githubtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubtest
+//
+//  Created by Troy Neumann on 2/16/18.
+//  Copyright © 2018 Mayo Clinic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
