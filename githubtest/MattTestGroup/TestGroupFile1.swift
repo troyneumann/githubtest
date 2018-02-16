a6126c3de7a37285f340d46dfd01514acf7f401f
m082123 <lien.matthew@mayo.edu>
2018-02-16 12:45:02 -0600

    add files and groups

---
+++ githubtest/MattTestGroup/TestGroupFile1.swift
@@ -0,0 +1,9 @@
+//
+//  TestGroupFile1.swift
+//  githubtest
+//
+//  Created by Lien, Matthew J. (Matt) on 2/16/18.
+//  Copyright © 2018 Mayo Clinic. All rights reserved.
+//
+
+import Foundation
---
+++ githubtest/MattTestViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MattTestViewController.swift
+//  githubtest
+//
+//  Created by Lien, Matthew J. (Matt) on 2/16/18.
+//  Copyright © 2018 Mayo Clinic. All rights reserved.
+//
+
+import Foundation
